8bb22ce24ed9564bbd974dc4fdec4fd31545bf25
kiselevm2023 <kiselevm2023@yandex.ru>
2024-03-19 23:12:50 +0400

    6 редакция ТЗ-14

--- src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -13,42 +13,43 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse onMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.debug("Status is received 400 Bad request {}", e.getMessage(), e);
         return new ErrorResponse(e.getFieldError().getDefaultMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleNotFoundException(final NotFoundException e) {
+        log.debug("Status is received 404 Not found {}", e.getMessage(), e);
         return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public ErrorResponse handleNotFoundException(final Forbidden e) {
+        log.debug("Status is received 403 Forbidden {}", e.getMessage(), e);
         return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleNotFoundException(final AlreadyExsist e) {
+        log.debug("Status is received 500 Internal server error {}", e.getMessage(), e);
         return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleThrowable(final ValidationException e) {
-        return new ErrorResponse(
-                e.getMessage()
-        );
+        log.debug("Status is received 400 Bad request {}", e.getMessage(), e);
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleThrowable(final Throwable e) {
-        log.debug(e.getMessage());
-        return new ErrorResponse(
-                "Произошла непредвиденная ошибка."
-        );
+        log.debug("Status is received 500 Internal server error {}", e.getMessage(), e);
+        return new ErrorResponse(e.getMessage());
     }
 
 }
